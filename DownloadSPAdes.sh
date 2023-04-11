183abab6bd908a161195995d1931215120f7f36d
Justin Bryan <jbb957@login03.rc.byu.edu>
2023-04-10 18:35:12 -0600

    Updated SPAdes files. Should be functional, but not yet tested.

--- DownloadSPAdes.sh
+++ DownloadSPAdes.sh
@@ -1,19 +1,25 @@
 #!bin/bash/
 
+DEBUG_PFX="DownloadSPAdes.sh:"
 SPADES_PATH=$1
 
 if [ -z "$SPADES_PATH"]
   then
-    echo "No path specified. Defaulting to: \"$PWD/spades/\"."
+    echo "$DEBUG_PFX No path specified. Defaulting to: \"$PWD/spades/\"."
     SPADES_PATH="$PWD/spades/"
 fi    
 
 # create directory if not exists
+mkdir -p $SPADES_PATH
+cd $SPADES_PATH
 
 # wget
+echo "$DEBUG_PFX Downloading SPAdes tar.gz..."
+wget http://cab.spbu.ru/files/release3.15.5/SPAdes-3.15.5-Linux.tar.gz
 
 # unpack tarball
+echo "$DEBUG_PFX Unpacking tar.gz..."
+tar -xzf SPAdes-3.15.5-Linux.tar.gz
+rm SPAdes-3.15.5-Linux.tar.gz
 
-# config
-
-echo "$SPADES_PATH"
+echo "$DEBUG_PFX Finished."
--- runMetaSPAdes.sh
+++ runMetaSPAdes.sh
@@ -1,15 +1,37 @@
 #!bin/bash
 
-INPUT_FILEPATH=""
-METASPADES_PY_PATH="/grphome/grp_microsophila/SPAdes-3.15.5-Linux/bin/metaspades.py"
+DEBUG_PFX="runMetaSPAdes.sh:"
+
+INPUT_PATH=$1
+OUTPUT_PATH=$2
+INPUT_1_FILENAME=$3
+INPUT_2_FILENAME=$4
+
+METASPADES_PY_PATH="$PWD/spades/SPAdes-3.15.5-Linux/bin/metaspades.py"
 OUTPUT_DIR="/grphome/grp_microsophila/compute/mspades_output/"
 INPUT_DIR="/grphome/grp_microsophila/compute/linvilla_frost/microbiome/"
 
-INPUT_1_FILENAME="${INPUT_DIR}microbiome_reads.1.fastq"
-INPUT_2_FILENAME="${INPUT_DIR}microbiome_reads.2.fastq"
+if [ -z INPUT_1_FILENAME ] then
+  echo "$DEBUG_PFX Specific output path NOT specified. Defaulting to \"${PWD}/spades_out\"."
+  mkdir -p ${PWD}/spades_out
+  OUTPUT_PATH="${PWD}/spades_out"
+fi
+
+if [ -z INPUT_1_FILENAME ] then
+  echo "$DEBUG_PFX Specific filename for input 1 NOT specified. Defaulting to \"microbiome_reads.1.fastq\"."
+  INPUT_1_FILENAME="microbiome_reads.1.fastq"
+fi
+
+if [ -z INPUT_2_FILENAME ] then
+  echo "$DEBUG_PFX Specific filename for input 2 NOT specified. Defaulting to \"microbiome_reads.2.fastq\"."
+  INPUT_2_FILENAME="microbiome_reads.2.fastq"
+fi
+
+INPUT_1_FILENAME="${INPUT_PATH}/${INPUT_1_FILENAME}"
+INPUT_2_FILENAME="${INPUT_PATH}/${INPUT_2_FILENAME}"
 
 
-full_cmd="$METASPADES_PY_PATH -o $OUTPUT_DIR -1 $INPUT_1_FILENAME -2 $INPUT_2_FILENAME"
+full_cmd="$METASPADES_PY_PATH -o $OUTPUT_PATH -1 $INPUT_1_FILENAME -2 $INPUT_2_FILENAME"
 echo $full_cmd
 eval $full_cmd
 
